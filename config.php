9914093b7ea41ce39f7c3b21383446587ebab19a
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-11-07 20:44:48 -0300

    setando o conifg para o ambiente de produção

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php 
 
-define('INCLUDE_PATH', 'http://localhost/gs7digital/another/');
-#define('INCLUDE_PATH', 'https://gs7digital.com.br/');
+#define('INCLUDE_PATH', 'http://localhost/gs7digital/another/');
+define('INCLUDE_PATH', 'https://gs7digital.com.br/');
